7bd6e751b2cd1461b648e616b8463a1adb0cab43
Ivan Lieskov <ileskov584@gmail.com>
2022-04-16 15:59:02 +0200

    Ropsten test network available in truffle configuration

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,13 @@
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
-const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
-const privateKeys = process.env.PRIVATE_KEYS || ""
 
 
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraKey = "7ed1ab3a40f245fe99bf090e6f804126";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 
 module.exports = {
@@ -14,17 +17,17 @@ module.exports = {
       port: 7545,
       network_id: "*"//  Match any network id
     },
-    kovan: {
-      provider: function() {
-        return new HDWalletProvider(
-          privateKeys.split(','), // Array of accounts private keys
-          `https://kovan.infura/io/v3/${process.env.INFURA_API_KEY}`
-          )
-      },
-      gas: 5000000,
-      gasPrice: 25000000000,
-      network_id: 42
-    }
+
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
   },
   contracts_directory: './src/contracts',
   contracts_build_directory: './src/abis/',
