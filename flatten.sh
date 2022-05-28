0fa650ca1739eaedf2442f1dd2b79bb4ce15cc03
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-05-28 22:19:50 +0200

    flatten.sh

--- flatten.sh
+++ flatten.sh
@@ -2,3 +2,4 @@
 rm -rf src/flats/*
 ./node_modules/.bin/truffle-flattener src/contracts/Token.sol > src/flats/Token_flat.sol
 ./node_modules/.bin/truffle-flattener src/contracts/Exchange.sol > src/flats/Exchange_flat.sol
+
