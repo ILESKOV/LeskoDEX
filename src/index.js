621a37c35b0a23dea7f9fbb3444dcad773476ac2
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-05-28 22:05:31 +0200

    index.js
    index.js with all configurations

--- src/index.js
+++ src/index.js
@@ -17,3 +17,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister()
+
