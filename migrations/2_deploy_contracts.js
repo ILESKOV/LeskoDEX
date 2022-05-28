ca503ffd684a7e389dd12279143822c2bd73ee8d
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-05-28 21:58:56 +0200

    Token.Sol and Exchange.sol migration
    Token.Sol and Exchange.sol migration truffle script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,6 @@ const Exchange = artifacts.require("Exchange");
 
 module.exports = async function(deployer) {
   const accounts = await web3.eth.getAccounts()
-
   await deployer.deploy(Token);
 
   const feeAccount = accounts[0]
