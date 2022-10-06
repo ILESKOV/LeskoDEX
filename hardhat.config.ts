1dcf749287a0f9d16322bb3f8668d4697dac5e70
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-10-06 15:41:48 +0200

    Delete hardhat.config.ts

--- hardhat.config.ts
+++
@@ -1,9 +0,0 @@
-import { HardhatUserConfig } from "hardhat/config"
-import "@nomicfoundation/hardhat-toolbox"
-
-const config: HardhatUserConfig = {
-    solidity: "0.8.17",
-    paths: { sources: "./src/contracts" },
-}
-
-export default config
