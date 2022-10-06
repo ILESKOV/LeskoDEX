7eac0dc4a75cb12bbd5e9b61a7f779e48a13f920
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-10-06 15:38:19 +0200

    ESKO.sol and LESKOdex.sol migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,12 @@
-const Token = artifacts.require("Token");
-const Exchange = artifacts.require("Exchange");
+const Token = artifacts.require("ESKO");
+const Exchange = artifacts.require("LESKOdex");
 
-module.exports = async function(deployer) {
-  const accounts = await web3.eth.getAccounts()
+module.exports = async function (deployer) {
+  const accounts = await web3.eth.getAccounts();
   await deployer.deploy(Token);
 
-  const feeAccount = accounts[0]
-  const feePercent = 10
+  const feeAccount = accounts[0];
+  const feePercent = 10;
 
-  await deployer.deploy(Exchange, feeAccount, feePercent)
+  await deployer.deploy(Exchange, feeAccount, feePercent);
 };
